2c9ca67fa674f842d244fd3c3e2647e369419b4f
David Höck <d.hoeck@alpin11.at>
2020-05-14 16:37:56 +0200

    add phone number bundle as dependent bundle

--- src/MollieBundle.php
+++ src/MollieBundle.php
@@ -3,10 +3,13 @@
 
 namespace CoreShop\Payum\MollieBundle;
 
+use Misd\PhoneNumberBundle\MisdPhoneNumberBundle;
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
+use Pimcore\HttpKernel\Bundle\DependentBundleInterface;
+use Pimcore\HttpKernel\BundleCollection\BundleCollection;
 
-class MollieBundle extends AbstractPimcoreBundle
+class MollieBundle extends AbstractPimcoreBundle implements DependentBundleInterface
 {
     use PackageVersionTrait;
 
@@ -16,5 +19,13 @@ class MollieBundle extends AbstractPimcoreBundle
     protected function getComposerPackageName(): string
     {
         return 'alpin11/payum-mollie-bundle';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function registerDependentBundles(BundleCollection $collection)
+    {
+        $collection->addBundle(new MisdPhoneNumberBundle());
     }
 }
